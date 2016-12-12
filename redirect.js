405986f919b737d39d6adda0b35c5ca4a11e343f
Song Zhang <yitengruntu12123@gmail.com>
2016-12-12 16:05:03 +0800

    change ajax style

--- redirect.js
+++ redirect.js
@@ -3,7 +3,7 @@
 * @Date:   2016-12-09 18:55:07
 * @Last Modified by:   Song Zhang
 <<<<<<< HEAD
-* @Last Modified time: 2016-12-12 14:51:31
+* @Last Modified time: 2016-12-12 16:04:38
 =======
 * @Last Modified time: 2016-12-10 16:29:45
 >>>>>>> e08d68150700f2217d8348ae5058a66d9cc6a928
@@ -47,14 +47,12 @@ function getQueryVariable(variable) {
 
 $(document).ready(function() {
   $("button").click(function() {
-    $.post(url,
-    {
-      code: getQueryVariable('code'),
-      state: '123'
-    },
-    function(data,status){
-      alert("Data: " + data + "\nStatus: " + status);
-    });
+    $.ajax({
+      url: url,
+      type: 'POST',
+      success: function(result){
+        console.log(result)
+      }});
     // $.getJSON(str,function(result) {
     //   $.each(result, function(i, field) {
     //     $("p").append(field + " ");
